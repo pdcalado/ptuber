7f9d9bccf4d96341e57aa914e5d1a4bec0265fcb
Pedro Calado <pdcalado@gmail.com>
2015-02-05 19:39:08 +0000

    Created script to create title from filename.

---
+++ create_title.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @userinput = <STDIN>;
+foreach my $line (@userinput) {
+    chomp($line);
+
+    my $dot = rindex($line, ".");
+    my $slash = rindex($line, "/");
+
+    my $title = "";
+
+    if ($dot <= $slash || $dot < 0 || $slash < 0 || $dot - $slash - 1 <= 0) {
+	$title = "Bad Name";
+    }
+    else {
+	$title = substr($line, $slash + 1, $dot - $slash - 1);
+    }
+
+    print $title . "\n";
+}
