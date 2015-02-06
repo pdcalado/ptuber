8e18a3f8f694301363998e31e8631f40c8a3c786
Pedro Calado <pdcalado@gmail.com>
2015-02-06 21:00:25 +0000

    Moved redis stuff into own dir. Added script to list all keys from namespace.

---
+++ redis/list_keys.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Redis::Client;
+
+# read stuff from config
+use Config::IniFiles;
+my $config = Config::IniFiles->new( -file => "config.ini" );
+
+# Database endpoint
+my $db_hostname = $config->val("DB", "Database Hostname");
+my $db_port = $config->val("DB", "Database Port");
+# Videos database prefix
+my $vid_prefix = $config->val("DB", "Videos Prefix");
+
+# Connect to database
+my $red = Redis::Client->new( host => $db_hostname,
+			      port => $db_port,
+			      name => "perl_connection");
+
+my @list = $red->keys($ARGV[0] . ":*");
+
+foreach my $item (@list) {
+    print substr($item, length($ARGV[0]) + 1) . "\n";
+}
