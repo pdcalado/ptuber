875ea5969fce96b1807fdbe7b9905a59789e8f13
Pedro Calado <pedro.calado@movvo.com>
2015-08-06 12:24:34 +0100

    Simplify encrypt script.

--- bash/encrypt.sh
+++ bash/encrypt.sh
@@ -2,8 +2,6 @@
 
 # $2 is password
 
-output=$(echo -n "$1" | md5sum - | cut -d' ' -f1)
+openssl aes-256-cbc -a -salt -in $1 -out $3 -k $2
 
-openssl aes-256-cbc -a -salt -in $1 -out "$output.enc" -k $2
-
-echo "Encrypted to $output.enc"
+echo "Encrypted to $3"
