b53281d1273b3df52b149b1e99e800de7cf2e057
Pedro Calado <pedro.calado@movvo.com>
2015-08-06 16:34:58 +0100

    Fix prepare upload script to handle thumbs.

--- bash/prepare_upload.sh
+++ bash/prepare_upload.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+if [ "$2" == "" ]
+then
+    echo "Missing argument"
+    exit 1
+fi
+
 hash_name=$(echo -n "$1" | md5sum - | cut -d' ' -f1)
 
 pass=$(bash gen_passwd.sh)
 
+# Check if encrypted file already exists
 nulled=$(stat $2/$hash_name >> /dev/null 2>&1)
 
 rc=$?
@@ -13,8 +20,26 @@ then
     exit 1
 fi
 
+# Check if thumbs already exist
+nulled=$(stat thumbs/$hash_name-* >> /dev/null 2>&1)
+
+rc=$?
+if [ $rc -eq 0 ]
+then
+    echo "Thumbs already exist"
+else
+    # Create thumbs
+    bash thumbs.sh $1
+fi
+
 # encrypt file
 bash encrypt.sh $1 $pass $2/$hash_name
 
+tar -czf $hash_name.temporary.tar.gz thumbs/$hash_name-*
+
+bash encrypt.sh $hash_name.temporary.tar.gz $pass $2/$hash_name.tar.gz
+
+rm -f $hash_name.temporary.tar.gz
+
 # register encrypted file in db
-echo "{\"id\": \"$hash_name\", \"name\": \"$1\", \"password\": \"$pass\", \"path\": \"$PWD$1\"}"
+curl --data "{\"id\": \"$hash_name\", \"name\": \"$1\", \"password\": \"$pass\", \"path\": \"$PWD$1\"}" localhost:3000/encrypted
