ff72e472632b6ec30b918a06df7a25e3e9f6a159
Pedro Calado <pdcalado@gmail.com>
2015-08-09 17:11:40 +0100

    Stop uploading if preparation fails.

--- bash/multiple_upload.sh
+++ bash/multiple_upload.sh
@@ -4,6 +4,14 @@ for item in $*; do
     echo "-- Processing $item --"
     pass=$(bash gen_passwd.sh)
     bash prepare_upload.sh $item crypted $pass
+
+    rc=$?
+
+    if [ $rc -ne 0 ]
+    then
+	continue
+    fi
+
     bash upload.sh $item Brno/ $pass
     echo "-- Ending --"
 done
