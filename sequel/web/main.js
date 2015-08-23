ef8b587b18eda670ba3188a855ccaf8c1c9e31c7
Pedro Calado <pdcalado@gmail.com>
2015-08-23 23:50:46 +0100

    Object blob will represent the clip in backbone.

--- sequel/web/main.js
+++ sequel/web/main.js
@@ -18,24 +18,26 @@ var AppView = Backbone.View.extend({
 var appView = new AppView();
 
 var app = {};
-app.Todo = Backbone.Model.extend({
+app.Blob = Backbone.Model.extend({
     defaults: {
-        title: '',
-        completed: false
+        id: "",
+        name: "",
+	password: ""
     }
 });
 
-app.TodoList = Backbone.Collection.extend({
-    model: app.Todo,
-    localStorage: new Store("backbone-todo")
+app.BlobList = Backbone.Collection.extend({
+    model: app.Blob,
+    url: 'http://localhost:3000/list',
+    // localStorage: new Store("backbone-todo")
 });
 
 // instance of the Collection
-app.todoList = new app.TodoList();
+app.blobList = new app.BlobList();
 
-var todo = new app.Todo({title: 'Learn Backbone.js', completed: false}); // create object with the attributes specified.
-console.log(todo.get('title')); // "Learn Backbone.js" 
-console.log(todo.get('completed')); // false
-console.log(todo.get('created_at')); // undefined
-console.log(todo.set('created_at', Date()));
-console.log(todo.get('created_at')); // "Wed Sep 12 2012 12:51:17 GMT-0400 (EDT)"
+// var todo = new app.Todo({title: 'Learn Backbone.js', completed: false});
+// console.log(todo.get('title')); // "Learn Backbone.js" 
+// console.log(todo.get('completed')); // false
+// console.log(todo.get('created_at')); // undefined
+// console.log(todo.set('created_at', Date()));
+// console.log(todo.get('created_at')); // "Wed Sep 12 2012 12:51:17 GMT-0400 (EDT)"
