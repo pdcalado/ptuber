83ed97c447dce819072e5691f19621f709adbe6e
Pedro Calado <pdcalado@gmail.com>
2015-08-23 15:58:41 +0100

    Renamed backup db folder.

--- bash/source_this.sh
+++ bash/source_this.sh
@@ -2,7 +2,7 @@
 
 export PATH=$PATH:/home/calado/deleteme/scripts
 export THUMBS_FOLDER=thumbs
-export BACKUP_FOLDER=backup
+export BACKUP_FOLDER=backupdb
 export DB_URL=localhost:3000
 export CRYPTED_FOLDER=crypted
 export DECRYPTED_FOLDER=decrypted
